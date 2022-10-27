618f99bd90b82ef727a17b07c2bd43195c69c17d
AppNetlink <app@netlink.vn>
2022-10-27 14:09:55 +0700

    first commit

---
+++ mylibrary/src/main/java/com/google/gms/mylibrary/NexusNew.java
@@ -0,0 +1,10 @@
+package com.google.gms.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class NexusNew {
+    public void NexxusToast(Context context, String string){
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+    }
+}
